223256a652543e5de71c28c81741f269c5bcdb04
kyukiho <3378391157@qq.com>
2025-02-26 15:29:13 +0800

    Site updated: 2025-02-26 15:29:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/25/categories/","2025/02/24/生活日常/流水账（大一上）/","2025/02/26/前端开发/使用JavaScript编写动态脚本/"];function toRandomPost(){
+var posts=["2025/02/26/前端开发/使用JavaScript编写动态脚本/","2025/02/24/生活日常/流水账（大一上）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
